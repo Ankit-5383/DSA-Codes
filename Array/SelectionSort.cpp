7c8e9f16a8300d234ee9f93ba404ebb7f2be2da3
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-01-07 20:36:35 +0530

    Update SelectionSort.cpp

--- Array/SelectionSort.cpp
+++ Array/SelectionSort.cpp
@@ -1,30 +1,30 @@
 #include<iostream>
 using namespace std;
-int selectionSort(int arr[],int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        for(int j=i+1;j<n;j++)
-        {
-            if(arr[i]>arr[j])
-            {
-                swap(arr[i],arr[j]);
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
             }
         }
+        swap(arr[min_idx], arr[i]);
     }
-    for(int k=0;k<n;k++)
-    {
-    cout<<arr[k]<<" ";
+}
+
+int main() {
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter the value: ";
+        cin >> arr[i];
+    }
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionSort(arr, n);
+    
+    for (int k = 0; k < n; k++) {
+        cout << arr[k] << " ";
     }
 
-}
-int main()
-{
-    int arr[5];
-    for(int i=0;i<5;i++){
-    cout<<"enter the value";
-    cin>>arr[i];
-    }
-    int n=sizeof(arr)/sizeof(arr[0]);
-    selectionSort(arr,n);
+    return 0;
 }
