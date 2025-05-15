d6f50a5c32a24639a590fd57019e7654e82243f8
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-05-16 00:21:25 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (9.44%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -1,29 +1,26 @@
 class Solution {
     int n;
 
+    vector<string> getAlternatingGroupBasedSubseq(const vector<string>& words, const vector<int>& groups, int prevGroup) {
+        vector<string> subseq;
+
+        for(int i = 0; i < n; ++i) {
+            if(groups[i] != prevGroup) {
+                subseq.push_back(words[i]);
+                prevGroup = groups[i];
+            }
+        }
+
+        return subseq;
+    }
+
 public:
     vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
         n = words.size();
 
-        int prev=0;
-        vector<vector<int>>subseq(2);
-        for(int i=0;i<n;i++){
-            if(prev!=groups[i]){
-                subseq[0].push_back(i);
-                prev=groups[i];
-            }
-        }
-        prev=1;
-        for(int i=0;i<n;i++){
-            if(prev!=groups[i]){
-                subseq[1].push_back(i);
-                prev=groups[i];
-            }
-        }  
+        vector<string> subseq1 = getAlternatingGroupBasedSubseq(words, groups, 0);
+        vector<string> subseq2 = getAlternatingGroupBasedSubseq(words, groups, 1);
 
-        vector<string> result;
-        for(int i : (subseq[0].size() > subseq[1].size() ? subseq[0] : subseq[1])) 
-            result.push_back(words[i]);
-        return result;
+        return (subseq1.size() > subseq2.size()) ? subseq1 : subseq2;
     }
 };
