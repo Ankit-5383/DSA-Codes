f1bb50fa17597528b4f5278a9b579b472a930fa7
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-05-18 22:42:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.82%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,35 +1,22 @@
 class Solution {
+    void moveTargetToRightPlace(vector<int>& nums, int target, int& i) {
+        int j = 0;
+        while(j < nums.size() && nums[j] != target)
+            j++;
+
+        while(j < nums.size()) {
+            if(nums[j] == target) {
+                swap(nums[i], nums[j]);
+                i++;
+            }
+            j++;
+        }
+    }
+
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-
-        // Find index of first zero
-        int j = 0;
-        while(j < n && nums[j] != 0) {   
-            j++;
-        }
-        // Place all the zeros at beginning of array
         int i = 0;
-        while(j < n) {
-            if(nums[j] == 0) {
-                swap(nums[i], nums[j]);
-                i++;
-            }
-            j++;
-        }
-
-        // Find index of first one
-        j = 0;
-        while(j < n && nums[j] != 1) {   
-            j++;
-        }
-        // Place all the ones at beginning of array
-        while(j < n) {
-            if(nums[j] == 1) {
-                swap(nums[i], nums[j]);
-                i++;
-            }
-            j++;
-        }
+        moveTargetToRightPlace(nums, 0, i);
+        moveTargetToRightPlace(nums, 1, i);
     }
 };
