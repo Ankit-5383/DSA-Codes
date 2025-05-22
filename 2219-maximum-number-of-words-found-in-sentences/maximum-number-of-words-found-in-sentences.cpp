5d01369f423e1944e775ebae0963f94ffff97b33
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-05-22 23:01:34 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (78.39%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -1,21 +1,19 @@
 class Solution {
+    int getSpaceCount(const string& str) {
+        int count = 0;
+        for(char ch : str) count += (ch == ' ');
+        return count;
+    }
+
 public:
-    int mostWordsFound(vector<string>& sentences) {
-        int MaxWordCount = 0;
-        for(int i =0; i<sentences.size(); i++){
-            string sentence = sentences[i];
-            int CurrentWordCount = 0;
-            for(int j =0; j<sentence.size();j++){
-                if(sentence[j]==' '||j==sentence.size()-1){
-                    CurrentWordCount++;
-                    if(CurrentWordCount>MaxWordCount)
-                    {
-                        MaxWordCount=CurrentWordCount;
-                    }
-                }
-            }
+    int mostWordsFound(const vector<string>& sentences) {
+        int maxWords = 0;
+
+        for(const auto& sen : sentences) {
+            int spaceCount = getSpaceCount(sen);
+            maxWords = max(maxWords, spaceCount);
         }
-        return MaxWordCount;
-        
+
+        return maxWords + 1;
     }
 };
