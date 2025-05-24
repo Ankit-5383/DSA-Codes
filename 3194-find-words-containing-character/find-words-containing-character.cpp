6ef8327d06c9f0b487a2a0c9cf0a7e123dcd38af
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-05-24 20:22:21 +0530

    Time: 0 ms (100.00%) | Memory: 33.7 MB (12.10%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    vector<int> findWordsContaining(vector<string>& word, char x) {
+    vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int>indices;
-
-        for(int i=0;i<word.size();i++){
-            for(int j=0;j<word[i].size();j++){
-                if(word[i][j]==x){
-                indices.push_back(i);
-                break;
+        int i=0;
+        for(string word:words){
+            for(char ch:word){
+                if(ch==x){
+                    indices.push_back(i);
+                    break;
                 }
             }
+            i++;
         }
         return indices;
     }
