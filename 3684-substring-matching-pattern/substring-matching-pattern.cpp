c2abc552980da263548d2e0f3af6124b93c0cd5b
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-05-28 20:55:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (32.54%) - LeetSync

--- 3684-substring-matching-pattern/substring-matching-pattern.cpp
+++ 3684-substring-matching-pattern/substring-matching-pattern.cpp
@@ -1,43 +1,34 @@
 class Solution {
-    string Before(string p) {
+    string findTheStar(string& p, bool toFindBefore) {
         string res;
-        for (int i = 0; i < p.size(); i++) {
+        for (int i = (toFindBefore ? 0 : p.size() - 1);(toFindBefore ? i < p.size() : i >= 0);(toFindBefore ? i++ : i--)) {
             if (p[i] == '*') {
                 break;
             }
             res.push_back(p[i]);
         }
-        return res;
-    }
-    string After(string p) {
-        string res;
-        for (int i = p.size() - 1; i >= 0; i--) {
-            if (p[i] == '*') {
-                break;
-            }
-            res.push_back(p[i]);
+        if (!toFindBefore) {
+            reverse(res.begin(), res.end());
         }
-        reverse(res.begin(), res.end());
         return res;
     }
+public : bool hasMatch(string s, string p) {
 
-public:
-    bool hasMatch(string s, string p) {
+    string befor = findTheStar(p,true);
+    string after = findTheStar(p,false);
 
-        string befor = Before(p);
-        string after = After(p);
-
-        size_t pos = s.find(befor);
-        if (pos != string::npos) {
-            int afterPos = s.find(after, pos + befor.size());
-            if (afterPos != -1)
-                return true;
-            else
-                return false;
-        } else {
+    size_t pos = s.find(befor);
+    if (pos != string::npos) {
+        int afterPos = s.find(after, pos + befor.size());
+        if (afterPos != -1)
+            return true;
+        else
             return false;
-        }
-
+    } else {
         return false;
     }
-};
+
+    return false;
+}
+}
+;
