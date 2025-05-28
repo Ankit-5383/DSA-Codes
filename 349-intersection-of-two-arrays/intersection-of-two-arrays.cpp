1f5a58e1ed5becd64fc48a4dea0167dd0e164f64
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-05-28 21:12:02 +0530

    Time: 4 ms (23.66%) | Memory: 14 MB (80.33%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -16,7 +16,8 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
 
         for (int i = 0; i < (sz1 < sz2 ? sz1 : sz2); i++) {
             for (int j = indexCount; j < (sz1 < sz2 ? sz2 : sz1); j++) {
-                if ((sz1 < sz2 ? nums1[i] == nums2[j] : nums2[i] == nums1[j])) {
+                if ((sz1 < sz2 ? nums1[i] == nums2[j] 
+                               : nums2[i] == nums1[j])) {
                     int val = count(result.begin(), result.end(), (sz1 < sz2 ? nums2[j] : nums1[j]));  
                     if (val == 0) {
                         result.push_back(sz1 < sz2 ? nums2[j] : nums1[j]);
