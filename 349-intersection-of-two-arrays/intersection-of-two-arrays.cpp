465184d7b568f964dbd71bcac9797db356ed191f
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-05-28 22:23:25 +0530

    Time: 3 ms (39.47%) | Memory: 14.4 MB (50.26%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,41 +1,16 @@
 class Solution {
-    int maxSize;
-
-   bool Findfreq( vector<int>check,int ele){
-    for(int num : check) if(num == ele) return true;
-    return false;
-    }
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-                if (nums1 == nums2) {
-            return nums1;
-        }
-        int n = nums1.size();
-        int m = nums2.size();
-        
+        int n = nums1.size(), m = nums2.size();
 
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
+        vector<int> commons, visited(1001);
 
-        maxSize = max(nums1[n - 1], nums2[m-1]) + 1;
-        vector<int> result;
-        vector<int>freq2(maxSize,0);
+        for(int num1 : nums1)
+            for(int num2 : nums2)
+                if(!visited[num1] && num1 == num2)
+                    commons.push_back(num1),
+                    visited[num1] = 1;
 
-        //for nums2
-        for(int i=0;i<m;i++){
-            int val=nums2[i];
-            freq2[val]++;
-        }
-
-        for (int i = 0; i < n; i++) {
-            int val=nums1[i];
-            if(freq2[val]>0){
-                if(!Findfreq(result,val)){
-                    result.push_back(val);
-                }
-            }
-        }
-
-        return result;
+        return commons;
     }
 };
