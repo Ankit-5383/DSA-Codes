0a08a1402d3b7e949b8602de7d11af7e49b7aeae
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-05-29 02:13:32 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (90.14%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -1,28 +1,13 @@
 class Solution {
-     bool isPresnt(vector<int>&nums ,int target){
-        int s=0;
-        int e=nums.size()-1;
-
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(nums[mid]==target){
-                return true;
-            }
-            else if(nums[mid]>target){
-                e=mid-1;
-            }else{
-                s=mid+1;
-            }
-        }
-        return false;
-     }
 public:
     int findFinalValue(vector<int>& nums, int original) {
+        vector<bool>isPresent(2001);
+        for(int val: nums){
+            isPresent[val]=true;
+        }
 
-        sort(nums.begin(),nums.end());
-        //1,3,5,6,12
         while(true){
-         if(isPresnt(nums,original)){
+         if(isPresent[original]){
             original=2*original;
          }else {
             break;
