d6f817447a3efbbded3d5b23e1b8299cde00b65b
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-05-30 14:21:04 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (49.87%) - LeetSync

--- 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.cpp
+++ 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.cpp
@@ -2,26 +2,17 @@ class Solution {
 public:
     bool isPrefixString(string s, vector<string>& words) {
         string check;
-        for(string ch: words){
+        
+        for (string ch : words) {
             check.append(ch);
+            
+           
+            if (check == s) return true;
+            
+        
+            if (check.size() > s.size()) return false;
         }
-        if(check.size()<s.size()){
-            return false;
-        }
-        int i=0;
-        int j=0;
-
-
-        while(i<s.size() && j<words.size()){
-            int sz= words[j].size();
-            string res= s.substr(i, sz);
-            if(res!=words[j]){
-                return false;
-            }
-            i=i+sz;
-            j++;
-        }
-
-       return true;
+        
+        return false;
     }
 };
