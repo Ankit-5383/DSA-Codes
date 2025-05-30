be953e1f1ddb834b99ff017e48f490cf5267e8ab
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-05-30 08:45:09 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (6.09%) - LeetSync

--- 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.cpp
+++ 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     vector<int> findIndices(vector<int>& nums, int idxDiff, int valDiff) {
-      
-        vector<int>ans={-1,-1};
-        for(int i=0;i<nums.size();i++){
-            for(int j=i;j<nums.size();j++){
-                if( abs(i - j) >= idxDiff && abs(nums[i]-nums[j])>=valDiff ){
-                   ans[0]= i;
-                   ans[1] = j;
-                   return ans;
-                }
-            }
+
+      vector<int>ans={-1,-1};
+      if(nums.size()==1 && idxDiff==0 &&valDiff==0 ){
+            ans[0] = 0;
+            ans[1] = 0;
+            return ans;
         }
-        return ans;
+      int i=0;
+      int j=nums.size()-1;
+      while(i < j){
+         if(abs(i-j)>=idxDiff  && abs(nums[i]-nums[j])>=valDiff){
+            ans[0]=i;
+            ans[1]=j;
+            return ans;
+         }
+           j--;
+         if (i >= j) {  
+            ++i;
+            j = nums.size() - 1;
+         }
+      }
+      return ans;
     }
 };
