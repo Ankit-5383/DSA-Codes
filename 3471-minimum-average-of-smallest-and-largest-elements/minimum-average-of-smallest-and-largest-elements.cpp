1ef763d4bb4f92f491668be3e97ba70e88380bce
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-06-04 08:22:01 +0530

    Time: 3 ms (17.24%) | Memory: 25.7 MB (64.70%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -1,43 +1,20 @@
 class Solution {
-    int toFindSmallEle( vector<int> &nums){
-    int idx=0;
-    int small = INT_MAX;
-    for(int i=0;i<nums.size(); i++){
-        if(small>nums[i]){
-            small=nums[i];
-            idx=i;
-        }
-    }
-    return idx ;  
-   }
-   int toFindLargestEle(vector<int> &nums){
-    int idx=0;
-    int larg = INT_MIN;
-    for(int i=0;i<nums.size(); i++){
-        if(larg<nums[i]){
-            larg=nums[i];
-            idx=i;
-        }
-    }
-    return idx ;  
-   }
 public:
     double minimumAverage(vector<int>& nums) {
-        double res =INT_MAX;
-        while(nums.size()){
-
-            int a= toFindSmallEle(nums);
-            int val1= nums[a];
-            nums.erase(nums.begin() + a);
-            int b= toFindLargestEle(nums);
-            int val2= nums[b];
-            nums.erase(nums.begin() + b);
-
-            double avg= (val1 + val2)/2.0;
-            res = min(res,avg);
-
-        }
         
+        double res=INT_MAX;
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=nums.size()-1;
+        while(i<j){
+             double val = (nums[i]+nums[j]) / 2.0;
+             if(res>val){
+                res=val;
+             }
+             i++;
+             j--;
+        }
+
         return res;
     }
 };
