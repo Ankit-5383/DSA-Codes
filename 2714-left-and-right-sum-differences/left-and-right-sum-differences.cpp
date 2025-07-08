ba89b18eb8f57e06b6ed9edd0f3658d9c40e7835
Ankit Kailas Chaudhari <chaudhariankit2005@gmail.com>
2025-07-08 23:39:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (35.63%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -1,31 +1,24 @@
 class Solution {
-    vector<int> gentratearr(int k, vector<int>& nums, int n) {
-        vector<int> result(n, 0);
-
-        int start = (k == 1) ? 1 : n - 2;
-        int end = (k == 1) ? n : -1;
-        int step = (k == 1) ? 1 : -1;
-
-        for (int i = start; i != end; i += step) {
-            int from = (k == 1) ? i - 1 : i + 1;
-            result[i] = result[from] + nums[from];
-        }
-
-        return result;
-    }
-
 public:
     vector<int> leftRightDifference(vector<int>& nums) {
         int n = nums.size();
+        vector<int> leftSum(n, 0);
+        vector<int> rightSum(n, 0);
 
-        vector<int> prefix = gentratearr(1, nums, n);
-        vector<int> suffix = gentratearr(0, nums, n);
+        leftSum[0] = 0;
+        rightSum[n - 1] = 0;
 
-        vector<int> result(n);
-        for (int i = 0; i < n; ++i) {
-            result[i] = abs(prefix[i] - suffix[i]);
+        int j = n - 2;
+        for (int i = 1; i < n; ++i) {
+            leftSum[i] = leftSum[i - 1] + nums[i - 1];
+            rightSum[j] = rightSum[j + 1] + nums[j + 1];
+            j--;
         }
 
-        return result;
+        for (int i = 0; i < n; ++i) {
+            nums[i] = abs(leftSum[i] - rightSum[i]);
+        }
+
+        return nums;
     }
 };
